a458cffc20b0604c668494b92279be101bc07ef3
NikitaSkachkov <145668677+NikitaSkachkov@users.noreply.github.com>
2024-03-14 15:25:29 +0100

    Time: 73 ms (54.28%) | Memory: 55.4 MB (8.81%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -5,15 +5,4 @@
  */
 var isAnagram = function(s, t) {
     return JSON.stringify(s.split('').sort()) === JSON.stringify(t.split('').sort())
-
-
-    // let collection1 = new Set(s);
-    // let test = true;
-    // t.split('').forEach(letter => {
-    //     if (!collection1.has(letter)) {
-    //         test = false;
-    //     }
-    // })
-    // if (test && s.length !== t.length) test = false;
-    // return test;
 };
