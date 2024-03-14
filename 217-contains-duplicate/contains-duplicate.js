54adbadc041780cb1155eecc70ab97e430c588be
NikitaSkachkov <145668677+NikitaSkachkov@users.noreply.github.com>
2024-03-14 14:40:10 +0100

    Time: 87 ms (34.97%) | Memory: 63.2 MB (19.74%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -3,6 +3,11 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
+
+    let arr = Array.from(new Set(nums));
+    return arr.length !== nums.length
+
+
     let isDuplicate = false;
     nums.sort();
     nums.forEach((num, index) => {
